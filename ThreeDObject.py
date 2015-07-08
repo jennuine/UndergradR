1b0076c4e29c02cfb30ce5ef2685f3373715f9d4
Jenn <jqnguyen@mix.wvu.edu>
2015-07-08 17:54:20 -0400

    nnsearch implemented

--- ThreeDObject.py
+++ ThreeDObject.py
@@ -1,31 +1,43 @@
 import glob
+import os
+import math
 import numpy as np
+import operator
 
-class ThreeDObject:
 
-    def __init__(self, name, filepath):
-        self.name = name
-        self.filepath = filepath
-        self.list = []
+def euclidianD(p1, p2):
+    x1, y1, z1 = p1
+    x2, y2, z2 = p2
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
 
-    def setList(self, x):
-        self.list.append(self.readOBJ(x))
 
-    def readOBJ(self, filepath):
+def nnsearch(a, b):
+    g = []
+    for ii in a:
+        l = []
+        for number, jj in enumerate(b):
+            l.append(euclidianD(ii, jj))
+        (m, i) = min((v, i) for i, v in enumerate(l))
+        g.append((m, i))
+    return g
+l = []
 
-        with open(filepath) as f:
-            content = f.readlines()
-        con1 = [ii.rstrip() for ii in content if len(ii) > 1]
-        print con1[0]
-        con2 = [each[2:] for each in con1 if each[0] == 'v']
-        print con2[0]
-        con7 = [each.split() for each in con2]
-        print con7[0]
-        con8 = [[float(X) for X in each] for each in con7]
-        print con8[0]
-        
-        l = np.asarray(con8)
-        return l
+with open('cellbody.obj') as f:
+    content = f.readlines()
 
-    def name():
-        return self.name
+con1 = [ii.rstrip() for ii in content if len(ii) > 1]
+con2 = [each[2:] for each in con1 if each[0] == 'v']
+con7 = [each.split() for each in con2]
+con8 = [[float(X) for X in each] for each in con7]
+cell = np.asarray(con8)
+
+with open('dendrites.obj') as f:
+    content1 = f.readlines()
+con3 = [ii.rstrip() for ii in content1 if len(ii) > 1]
+con4 = [each[2:] for each in con3 if each[0] == 'v']
+con5 = [each.split() for each in con4]
+con6 = [[float(X) for X in each] for each in con5]
+dend = np.asarray(con6)
+
+answer = nnsearch(cell, dend)
+print answer
--- obj2adj.py
+++ obj2adj.py
@@ -5,29 +5,35 @@ import numpy as np
 import operator
 import sys
 from termcolor import cprint
+import scipy.spatial.distance as scipy
 #import ThreeDObject
 
 sprint = lambda x : cprint('\t' + x, 'magenta')#, attrs=['bold'])
 rprint = lambda x : cprint('\t\t' + x, 'red')
-sprint("syst.arv => %s" % sys.argv)
-
+cyan = lambda x: cprint(x, 'cyan')
 
 class ThreeDObject:
 
     def __init__(self, name, filepath):
+        """
+        Constructor: creates new ThreeDObject
+        """
         self.name = name
         self.filepath = filepath
-        self.list = []
-        self.setList(filepath)
+        self.list = self.extractOBJ(filepath)
 
-    def setList(self, filepath):
-        self.list.append(self.extractOBJ(filepath))
+    def __str__(self): return self.name
+    def __repr__(self): return str(self.list)
 
     def extractOBJ(self, filepath):
-
+        """
+        Returns an array containing (x, y, z) coordinates for the .obj vertices
+        """
         with open(filepath) as f:
             content = f.readlines()
         
+        #cyan(content)
+
         con1 = [ii.rstrip() for ii in content if len(ii) > 1]
         sprint("con1 => %s" %con1[0])
         con2 = [each[2:] for each in con1 if each[0] == 'v']
@@ -38,20 +44,26 @@ class ThreeDObject:
         sprint("con8 => %s" %con8[0])
         
         l = np.asarray(con8)
+        #cyan(l)
         return l
 
     def getName(self): return self.name
-
     def getList(self): return self.list
 
 
 def euclidianD(p1, p2):
+    """
+    Returns the euclidian distance between p1 and p2 points
+    """
     x1, y1, z1 = p1
     x2, y2, z2 = p2
     return math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
 
 
 def nnsearch(a, b):
+    """
+    Returns an matrix of shortest paths from a and b
+    """
     g = []
     for ii in a:
         l = []
@@ -61,23 +73,26 @@ def nnsearch(a, b):
         g.append((m, i))
     return g
 
-var = ThreeDObject('cell', 'cellbody.obj')
-var.setList('cellbody.obj')
+def store(args):
+    """
+    Returns an array of ThreeDObjects based on filepath contained in args
+    """
+    objs = []
 
-rprint("var.getName() => %s" %var.getName())
+    for path in args[1:]:
+        obj = ThreeDObject(path.rstrip('.obj'), path)
+        objs.append(obj)
+        rprint("obj => %s" %obj)
+    return np.asarray(objs)
 
-"""k = f[0]
-h = f[1]
+objs = store(sys.argv)
 
+#cyan(objs)
 
+obj1 = objs[0].getList()
+obj2 = objs[1].getList()
 
-with open(h) as f:
-    content1 = f.readlines()
-con3 = [ii.rstrip() for ii in content1 if len(ii) > 1]
-con4 = [each[2:] for each in con3 if each[0] == 'v']
-con5 = [each.split() for each in con4]
-con6 = [[float(X) for X in each] for each in con5]
-dend = np.asarray(con6)
+answer = obj1.
+print answer
+cyan(answer)
 
-answer = nnsearch(cell, dend)
-print answer"""
